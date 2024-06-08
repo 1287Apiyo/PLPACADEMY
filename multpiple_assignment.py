85ab3b97607cb314e46f95ce84742011e4ea944e
Anne Apiyo <anneapiyo0@gmail.com>
2024-06-08 21:37:23 +0300

    Add files via upload

---
+++ multpiple_assignment.py
@@ -0,0 +1,17 @@
+#multiple assignment allows us  to assign multiple variabess  at the same time in ine line
+name="Anne"
+age=21
+attractive=True
+
+#multiple assignment
+name,age,attractive="Anne",21,True
+print(name)
+print(age)
+print(attractive)
+
+#example
+Spongebob=Patric=Sandy=Suidward=30
+print(Spongebob)
+print(Sandy)
+print(Patric)
+print(Suidward)
