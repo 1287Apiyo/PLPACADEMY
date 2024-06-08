42bbe084199317ea9ec3941fd88ee87dec271d3d
Anne Apiyo <anneapiyo0@gmail.com>
2024-06-08 21:30:37 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+
+#String Data Type
+first_name= "Anne"
+last_name="Apiyo"
+full_name=first_name+" "+last_name
+print(full_name)
+#INT Data Type
+age=21
+age=age+1
+print(age)
+print(type(age))
+#float data type
+height=250.5
+print ("Your height is: "+str(height))
+#print(type(height))
+#boolean
+human= True
+print("Are you a human: "+str(human))
+print(type(human))
+
+
