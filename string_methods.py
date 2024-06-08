f41e4840484fb400a51423d2cd29e70e9dd03ba4
Anne Apiyo <anneapiyo0@gmail.com>
2024-06-08 21:48:16 +0300

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+name="Anne"
+#print length of the variable name
+print(len(name))
+#method used to find a character within the string
+print(name.find("e")) 
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("e"))
+print(name.replace("a","o"))
+print(name*3)
